5b16f0d7200c7851a749e28ebd604e6651c88034
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-26 14:15:56 +0100

    Ejercicio 3 xml terminado

--- ej3xml.py
+++ ej3xml.py
@@ -7,17 +7,28 @@ arbol=etree.parse('ejercicioxml.xml')
 
 raiz=arbol.getroot()
 
+print ""
+#Pedimos la direccion del accidente por teclado
 direccion=raw_input("Dime una dirección: ")
+#Buscamos informacion referida a los accidentes
 direcciones=raiz.xpath("///accidente")
 
+#Recorremos la lista de direcciones
 for lugar in direcciones:
+	#Buscamos la coincidencia pedida por teclado
 	if lugar.find("firstAddress").text.startswith(direccion.upper()): 
-	#if lugar.find("firstAddress").text in direccion.upper():
+		#Imprimimos la informacion que estamos buscando
+		print ""
 		print "LUGAR accidente: ",lugar.find("firstAddress").text
 		print "TIPO accidente: ",lugar.find("type").text
 		print "RAZON accidente: ",lugar.find("reason").text
+		print ""
+
+	#Buscamos la coincidencia pedida por teclado	
 	elif lugar.find("firstAddress").text.endswith(direccion.upper()):
+		#Imprimimos la informacion que estamos buscando
+		print ""
 		print "LUGAR accidente: ",lugar.find("firstAddress").text
 		print "TIPO accidente: ",lugar.find("type").text
 		print "RAZON accidente: ",lugar.find("reason").text
-		
+		print ""
