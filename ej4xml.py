ab9da45b583e952d2ad8f681ccd618963d2df2d4
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-26 13:00:30 +0100

    Ejercicio 4 xml terminado

--- ej4xml.py
+++ ej4xml.py
@@ -7,11 +7,19 @@ arbol=etree.parse('ejercicioxml.xml')
 
 raiz=arbol.getroot()
 
+print ""
+#Pedimos la id del afectado por teclado
 afectadoid=raw_input("Dime la id del afectado: ")
+#Buscamos informacion referente a los afectados
 afectados=raiz.xpath("////afectado/afectado")
 
+print ""
+#Recorremos la lista de afectados
 for afec in afectados:
+	#Buscamos la coincidencia de la id pedida por teclado en la lista
 	if afec.find("id").text == afectadoid: 
-		print afec.find("id").text
-		print afec.find("age").text
-		print afec.find("status").text
+		#Imprimimos la información que estamos buscando
+		print "ID del afectado: ",afec.find("id").text
+		print "EDAD del afectado: ",afec.find("age").text
+		print "ESTADO del afectado: ",afec.find("status").text
+	
