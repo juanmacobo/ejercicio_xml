a37501457dae1a2d2ee269e1960f2d8e08c3ddc4
juanmacobo <juanma_cj_10@hotmail.com>
2016-02-27 21:41:12 +0100

    Ejercicio 5 xml a mejorar

--- ej5xml.py
+++ ej5xml.py
@@ -7,13 +7,17 @@ arbol=etree.parse('ejercicioxml.xml')
 
 raiz=arbol.getroot()
 
-numero=raiz.xpath("//accidente")
 accidentes=raiz.xpath("////afectado")
 
 afectados=[]
-cont=0
-for afectado in accidentes:
-	if len(accidentes) >=2:
-		cont=cont+1
-		print cont
-		
+
+for afec in accidentes:
+	if not afec.tag in afectados:
+		afectados.append(afec.tag)
+for num in afectados:
+	cont=0
+	numafec=raiz.xpath("////afectado/afectado")
+	for numero in numafec:
+		if numero.tag > 1:
+			cont=cont+1	
+	print cont			
