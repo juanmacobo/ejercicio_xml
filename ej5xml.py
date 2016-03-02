c0b5389b25c78912f75540d8a49b247c8c93d2b7
juanmacobo <juanma_cj_10@hotmail.com>
2016-03-02 19:15:35 +0100

    Ejercicio 5 xml terminado

--- ej5xml.py
+++ ej5xml.py
@@ -7,17 +7,26 @@ arbol=etree.parse('ejercicioxml.xml')
 
 raiz=arbol.getroot()
 
+#Buscamos informacion referente a los afectados
 accidentes=raiz.xpath("////afectado")
 
-afectados=[]
+#Creamos un contador para saber el numero de accidentes que tienen mas de un afectado
 contador=0
+#Recorremos la lista de afectados
 for afec in accidentes:
+	#Creamos un contador para saber cuantos afectados tiene cada accidente
 	cont=0
+	#Recorremos la lista de afectados que hay dentro de los afectados
 	for numero in afec:
+		#Ponemos una condicion al numero de afectados por accidente
 		if len(numero) > 1:
+			#Suma uno si se cumple la condicion
 			cont=cont+1	
+	#Ponemos una condicion para	el numero de accidentes con mas de un afectado	
 	if cont>1:
-		#print cont
+		#Suma uno se cumple la condicion
 		contador=contador+1
-print "Hay un total de",contador,"accidentes con mas de un afectado"
-
+#Imprimimos el resultado de la busqueda		
+print ""
+print "Hay un total de",contador,"accidentes que tienen más de un afectado."
+print ""
