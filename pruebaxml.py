620b87695c499f97868dad9edf019b77466e5a1d
juanmacobo <juanma_cj_10@hotmail.com>
2016-03-07 14:08:40 +0100

    Ejercicio prueba xml

--- pruebaxml.py
+++ pruebaxml.py
@@ -2,7 +2,8 @@
 
 #Prueba. Programa que pida un año y genere un fichero html con la siguiente información.
 
-f=open("index.html","w")
+fichero="index.html"
+f=open(fichero,"w")
 
 from lxml import etree
 arbol=etree.parse('ejercicioxml.xml')
@@ -14,9 +15,12 @@ anio=raw_input("Dime un año: ")
 accidentes=raiz.xpath("///accidente")
 
 for a in accidentes:
-	if a.find("year").text == anio:
-		f.write("<h1>",a.find("type").text,"</h1>","\n")
-		f.write("<p>",a.find("reason").text,"</p>","\n")
-		f.write("<ul>","\n")
-		f.write("<li>",raiz.find("result/accidente/afectado/afectado/age").text,"</li>","\n")
-		f.write("</ul>","\n")
+	if a.find("year").text == anio:	
+		f.write("<h1>"+a.find("type").text.encode("utf-8")+"</h1>"+"\n")
+		f.write("<h1>"+a.find("type").text.encode("utf-8")+"</h1>"+"\n")
+		f.write("<p>"+a.find("reason").text.encode("utf-8")+"</p>"+"\n")
+		f.write("<ul>"+"\n")
+		f.write("<li>"+raiz.find("result/accidente/afectado/afectado/age").text+"</li>"+"\n")
+		f.write("<li>"+raiz.find("result/accidente/afectado/afectado/type").text+"</li>"+"\n")
+		f.write("<li>"+raiz.find("result/accidente/afectado/afectado/status").text+"</li>"+"\n")
+		f.write("</ul>")
