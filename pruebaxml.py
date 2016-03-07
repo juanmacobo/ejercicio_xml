5d765ef4c78efec81ab7b4380d45cb7eadea1d71
juanmacobo <juanma_cj_10@hotmail.com>
2016-03-07 13:32:18 +0100

    Ejercicio prueba xml

--- pruebaxml.py
+++ pruebaxml.py
@@ -15,8 +15,8 @@ accidentes=raiz.xpath("///accidente")
 
 for a in accidentes:
 	if a.find("year").text == anio:
-		print "<h1>",a.find("type").text,"</h1>"
-		print "<p>",a.find("reason").text,"</p>"
-		print "<ul>"
-		print "<li>",raiz.find("result/accidente/afectado/afectado/age").text,"</li>"
-		print "</ul>"
+		f.write("<h1>",a.find("type").text,"</h1>","\n")
+		f.write("<p>",a.find("reason").text,"</p>","\n")
+		f.write("<ul>","\n")
+		f.write("<li>",raiz.find("result/accidente/afectado/afectado/age").text,"</li>","\n")
+		f.write("</ul>","\n")
